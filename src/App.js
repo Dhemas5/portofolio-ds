b00a20d1cf347a8c764bada463340c6f2891c0ec
Dhemas5 <Dhemas@example.com>
2024-01-11 06:19:38 +0700

    dhemas first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import AboutMe from './components/Aboutme';
+// import Projects from './components/Projects';
+import Profile from './components/Profile';
+import AboutBg from './components/AboutBg';
+import Rp from './components/Rp';
+import RpBg from './components/RpBg';
+import Footer from './components/Footer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <AboutMe />
+      <AboutBg />
+      <Profile />
+      <RpBg />
+      <Rp />
+      {/* <Projects /> */}
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
